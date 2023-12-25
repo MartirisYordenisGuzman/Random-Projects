793207aacf9982be9c8f20c04e761a1f5f2f0bbb
MartirisYordenisGuzman <72704662+MartirisYordenisGuzman@users.noreply.github.com>
2023-12-25 17:15:12 -0400

    Not important changes

--- Bootcamp-Project/frontend/src/App.js
+++ Bootcamp-Project/frontend/src/App.js
@@ -1,7 +1,17 @@
 
-
 function App() {
-  return <h1>Hola</h1>
+  return (
+  <>
+  <h1>Hola</h1>
+  <ElementP />
+  </>
+  );
+}
+
+
+function ElementP() {
+  return <p>This is a paragraph</p>
 }
 
 export default App;
+
